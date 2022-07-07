07bf4cbcfdadd96dfb9dcbe13761ddb963ffd8d5
jesselizcb <choque101108@gmail.com>
2022-07-07 00:12:59 -0500

    add amplify and authenticator

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "noteapp4136e10f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
